c32bc1cc8416c7597c2441924af90575e6273b93
Alok Nath Aiden <aloknathp675@gmail.com>
2020-01-06 19:38:59 +0000

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Creating a new Dir to move the existing .war file"
 mkdir -p /home/ubuntu/CI
-mv /home/ubuntu/workspace/CI/target/wezvaweb-1.0-SNAPSHOT.war /home/ubuntu/CI//wezvaweb-1.0-SNAPSHOT$(date +%F-%H:%M).war
+mv /home/ubuntu/workspace/CI/target/wezvaweb-1.0-SNAPSHOT.war /home/ubuntu/CI/wezvaweb-1.0-SNAPSHOT-$(date +%F-%H:%M).war
