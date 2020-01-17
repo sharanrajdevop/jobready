f67e185b08729efdbbb44811b51ad00303914439
Alok Nath Aiden <aloknathp675@gmail.com>
2020-01-17 12:26:43 +0000

    updating the same with the basic module

--- mvnstatus.sh
+++ mvnstatus.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-mvn clean package
-
+mvn package
 mvn_rtn=$?
-
 if [ "${mvn_rtn}" -eq 0 ]; then
-
-#git rev-parse --verify HEAD
-#git log -1 | grep "commit" |awk '{$1=="commit"; print $2}'
 echo "This Build is success ${mvn_rtn}"
 
 sudo cat > CI.json <<EOF
@@ -15,12 +10,9 @@ sudo cat > CI.json <<EOF
 "Date" : "$(date +%Y%m%d-%H:%M:%S)", "LSB" : "$(git rev-parse --verify HEAD)", "Branch" : "$(git rev-parse --abbrev-ref HEAD)"
 }
 EOF
-curl -XPOST 'http://172.31.13.88:9200/classes/class/1/' -H "Content-Type: application/json" -d'@CI.json'
-#curl -u 'USERNAME:PASSWORD' -XPOST 'http://localhost:9200/classes/class/4/'  -H "Content-Type: application/json" -d'@CI.json'
-curl -XGET  http://localhost:9200/classes/class/1/?pretty
-#curl -XGET 'http://localhost:9200/classes/class/_search?pretty=true&_source=LSB'
 
-mv CI.json CI.date.json
+curl -XPOST 'http://172.31.13.88:9200/classes/class/1/' -H "Content-Type: application/json" -d'@CI.json'
+curl -XGET  http://localhost:9200/classes/class/1/?pretty
 
 else
     echo "Maven Buildis Unsuccessful - with Retuned failure code ${mvn_rtn}"
