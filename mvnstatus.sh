445c4096b9ecd758ce83a3a7f1b7048c188ecc69
Alok Nath Aiden <aloknathp675@gmail.com>
2020-01-17 12:40:49 +0000

    Updated cat >>CI.json

--- mvnstatus.sh
+++ mvnstatus.sh
@@ -4,8 +4,7 @@ mvn package
 mvn_rtn=$?
 if [ "${mvn_rtn}" -eq 0 ]; then
 echo "This Build is success ${mvn_rtn}"
-
-sudo cat > CI.json <<EOF
+sudo cat >> CI.json <<EOF
 {
 "Date" : "$(date +%Y%m%d-%H:%M:%S)", "LSB" : "$(git rev-parse --verify HEAD)", "Branch" : "$(git rev-parse --abbrev-ref HEAD)"
 }
