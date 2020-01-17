cff04c93aef21babb73f012582992b4580743041
Alok Nath Aiden <aloknathp675@gmail.com>
2020-01-17 12:14:41 +0000

    Update mvnstatus.sh

--- mvnstatus.sh
+++ mvnstatus.sh
@@ -14,7 +14,7 @@ sudo cat >> CI.json <<EOF
 "Date" : "$(date +%Y%m%d-%H:%M:%S)", "LSB" : "$(git rev-parse --verify HEAD)", "Branch" : "$(git rev-parse --abbrev-ref HEAD)"
 }
 EOF
-curl -XPOST http://172.31.13.88:9200/classes/class/1/ -H "Content-Type: application/json" -d'@CI.json'
+curl -XPOST 'http://172.31.13.88:9200/classes/class/1/' -H "Content-Type: application/json" -d'@CI.json'
 #curl -u 'USERNAME:PASSWORD' -XPOST 'http://localhost:9200/classes/class/4/'  -H "Content-Type: application/json" -d'@CI.json'
 curl -XGET  http://localhost:9200/classes/class/1/?pretty
 #curl -XGET 'http://localhost:9200/classes/class/_search?pretty=true&_source=LSB'
